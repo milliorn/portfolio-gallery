ad5229714b4d1c7260dfa8d9f8f515d544a080ff
Philip Milliorn <scottmilliorn@outlook.com>
2020-12-10 15:55:51 -0800

    various config edits

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,9 @@ module.exports = {
             'Designing & building web and desktop apps with .NET along with websites in ReactJS.',
         author: 'Scott Milliorn',
         image: 'src/img/logo.jpg',
-        siteUrl: `https://example.com`, // Remove and fix this later
+        siteUrl: `https://scottmilliorn.netlify.app/`,
         social: {
-            twitter: '@scottmilliorn',
-            fbAppId: '100009436144652' //  Remove in future
+            twitter: '@scottmilliorn'
         }
     },
 
@@ -60,17 +59,6 @@ module.exports = {
                 head: false
             }
         },
-        /*{
-            resolve: `gatsby-plugin-purgecss`,
-            options: {
-                printRejected: true, // Print removed selectors and processed file names
-                develop: true // Enable while using `gatsby develop`
-                // tailwind: true, // Enable tailwindcss support
-                // whitelist: ['whitelist'], // Don't remove this selector
-                // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-                // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-            }
-        },*/
         `gatsby-plugin-preact`,
         `gatsby-plugin-minify`
     ]
