ced3162691a432073293317e2b0db25a782c8def
Philip Milliorn <scottmilliorn@outlook.com>
2020-12-12 21:31:43 -0800

    forcedReload

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,13 @@
 Detect SW updates
 https://github.com/gatsbyjs/gatsby/issues/9087
 */
+
+//Inform plugins when a service worker has been updated in the background and the page is ready to reload to apply changes.
 export function onServiceWorkerUpdateReady() {
-    return window.location.reload(true);
+    return window.location.forcedReload(true);
+}
+
+//Inform plugins of when a service worker has an update available.
+export function onServiceWorkerUpdateFound() {
+    return window.location.forcedReload(true);
 }
