c7ad74e4306bba45e013d12c5c74228ec6e15fee
Philip Milliorn <scottmilliorn@outlook.com>
2020-12-13 19:46:55 -0800

    updated gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,11 +4,7 @@ https://github.com/gatsbyjs/gatsby/issues/9087
 */
 
 //Inform plugins when a service worker has been updated in the background and the page is ready to reload to apply changes.
-export function onServiceWorkerUpdateReady() {
-    return window.location.forcedReload(true);
-}
+exports.onServiceWorkerUpdateReady = () => window.location.forcedReload(true);
 
 //Inform plugins of when a service worker has an update available.
-export function onServiceWorkerUpdateFound() {
-    return window.location.forcedReload(true);
-}
+exports.onServiceWorkerUpdateFound = () => window.location.forcedReload(true);
