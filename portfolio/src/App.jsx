fc6cfb6b1904d1bd6919c7c45a9401f175596da5
Scott Milliorn <scottmilliorn@gmail.com>
2022-04-04 23:48:22 -0700

    need to center h1

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,27 @@
 import "./App.css";
+import hero from "./images/hero.jpg";
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="mx-auto 2xl:container">
+      <div className="relative">
+        <img src={hero} alt="hero" className="absolute w-screen h-100" />
+        <div class="table w-full z-10 relative text-white">
+          <div class="table-header-group">
+            <div class="table-row">
+              <div class="table-cell text-center">Home</div>
+              <div class="table-cell text-center">About</div>
+              <div class="table-cell text-center">Skills</div>
+              <div class="table-cell text-center">Contact</div>
+            </div>
+          </div>
+        </div>
+        <div class="relative">
+          <h1 className="absolute w-screen text-3xl text-center text-white h-100">
+            React Developer
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 }
