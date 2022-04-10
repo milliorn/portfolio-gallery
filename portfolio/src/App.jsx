bc19ca905688f3b5d1a2bb420a713ced13c87d96
Scott Milliorn <scottmilliorn@gmail.com>
2022-04-10 16:05:56 -0700

    Projects (#241)
    * flex, order-2
    
    * refactor
    
    * done

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,31 +1,46 @@
 import "./App.css";
 import aboutImage from "./images/about.jpg";
 import { DiReact, DiJavascript1, DiHtml5, DiCss3 } from "react-icons/di";
-import { SiCsharp, SiTailwindcss, SiGatsby, SiBootstrap } from "react-icons/si";
+import {
+  SiCsharp,
+  SiTailwindcss,
+  SiGatsby,
+  SiBootstrap,
+  SiPostcss,
+  SiRust,
+  SiDocker,
+  SiGit,
+} from "react-icons/si";
 
 export default function App() {
   return (
-    <div className="mx-auto text-white 2xl:container">
+    <div className="mx-auto font-sans text-white 2xl:container">
       <nav
         id="navbar"
-        className="fixed z-10 table w-full p-1 mx-0 bg-black 2xl:container drop-shadow-2xl"
+        className="fixed z-10 table w-full px-0 py-2 mx-0 bg-black 2xl:container drop-shadow-2xl"
       >
         <div className="table-header-group">
-          <div className="table-row text-base capitalize">
+          <div className="table-row text-lg capitalize divide-x">
             <a href="/" className="table-cell text-center">
-              <span className="">home</span>
+              <span className="">h</span>
+              <span className="text-sm">ome</span>
             </a>
             <a href="/" className="table-cell text-center">
-              <span>about</span>
+              <span className="">a</span>
+              <span className="text-sm">bout</span>
             </a>
             <a href="/" className="table-cell text-center">
-              <span>skills</span>
+              <span className="">p</span>
+              <span className="text-xs">rojects</span>
             </a>
             <a href="/" className="table-cell text-center">
-              <span>projects</span>
+              <span className="">s</span>
+              <span className="text-xs">kills</span>
             </a>
+
             <a href="/" className="table-cell text-center">
-              <span>contact</span>
+              <span className="">c</span>
+              <span className="text-xs">ontact</span>
             </a>
           </div>
         </div>
@@ -33,7 +48,7 @@ export default function App() {
 
       <header
         id="hero"
-        className="h-screen w-screen bg-[url('./images/hero.jpg')] bg-cover bg-no-repeat 2xl:container"
+        className="h-screen w-full bg-[url('./images/hero.jpg')] bg-cover bg-no-repeat 2xl:container"
       >
         <div className="grid content-center justify-center w-screen h-screen 2xl:container">
           <span className="text-4xl text-white capitalize ">
@@ -42,60 +57,263 @@ export default function App() {
         </div>
       </header>
 
-      <section id="about">
+      <section id="about" className="bg-neutral-900">
         <div className="items-center w-screen mx-auto my-4 2xl:container sm:flex sm:flex-row">
           {/*<!--Left Col-->*/}
           <div className="flex flex-col items-start justify-center w-full p-8 text-center text-white lg:w-1/2">
-            <h1 className="mx-auto my-0 text-4xl font-semibold capitalize">
+            <h1 className="mx-auto my-0 text-4xl font-semibold leading-loose capitalize">
               <span className="">Scott Milliorn</span>
             </h1>
-            <p className="mx-auto my-4 leading-normal">
+            <p className="mx-auto my-2 leading-normal">
               Passionate developer. Experienced game developer. Focused on
               mobile first web development using React and Tailwind. Want to
               know more? Download my resume by clicking on the button.
             </p>
-            <button className="p-2 mx-auto my-0 text-white capitalize bg-gray-500 border border-gray-900 rounded shadow hover:animate-pulse hover:bg-gray-400 hover:text-gray-50 hover:shadow-lg hover:border-transparent">
+            <button className="p-2 mx-auto my-4 text-white capitalize bg-gray-500 border border-gray-900 rounded shadow outline outline-offset-1 outline-white hover:animate-pulse hover:bg-gray-400 hover:text-gray-50 hover:shadow-lg hover:border-transparent">
               resume
             </button>
           </div>
           {/*<!--Right Col-->*/}
-          <div className="w-screen px-4 ">
+          <div className="w-screen px-4 sm:w-full sm:px-0 sm:pr-4">
             <img src={aboutImage} alt="about" className="" />
           </div>
         </div>
       </section>
 
-      <section id="skills" className="bg-white">
-        <div className="p-4 mx-auto my-0">
-          <p className="text-4xl font-semibold text-center text-black capitalize">
-            Click an icons to see my work
+      <section id="projects" className="pt-4 m-4 bg-neutral-900">
+        <h1 className="text-2xl text-center capitalize ">
+          open source projects
+        </h1>
+
+        <div className="m-4 sm:flex">
+          <div className="relative border rounded-lg shadow-md max-w-fit bg-neutral-100 border-neutral-500">
+            <div className="p-5">
+              <h5 className="mb-2 text-lg font-bold text-center text-gray-900 capitalize">
+                Neverwinter Nights .NET
+              </h5>
+              <p className="mb-3 text-sm text-center text-neutral-900">
+                Open source organization for Neverwinter Nights: Enhanced
+                Edition. We have developed an API called Anvil for the
+                Neverwinter Nights Server. NWN.Masterlist is used to consume
+                Master Server API. NWN.Core is used to interlop functions for
+                NWNX .Net and wrap core functions. NWN.Native is a Low-level
+                NWNXLib bindings for managed to native interop. lorem
+              </p>
+              <br />
+              <div className="absolute bottom-0 left-0 right-0 m-1 text-center">
+                <a
+                  className="inline-flex items-center justify-center px-3 py-2 text-sm font-medium text-center capitalize rounded-lg text-neutral-50 bg-neutral-700 neutral:bg-neutral-900 focus:ring-4 neutral:ring-neutral-700"
+                  href="https://github.com/nwn-dotnet"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  learn more
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="m-4 sm:flex">
+          <div className="relative border rounded-lg shadow-md max-w-fit bg-neutral-100 border-neutral-500">
+            <div className="p-5">
+              <h5 className="mb-2 text-lg font-bold text-center text-gray-900 capitalize">
+                Gatsby Recipe Page
+              </h5>
+              <p className="mb-3 text-sm text-center text-neutral-900">
+                Recipe website done in GatsbyJS full stack frontend framework.
+                It uses Contentful CMS, source controlled on Github, built in
+                Gatsby Cloud and deployed onto Netlify. It also serves as my
+                personal site for cooking recipes.
+              </p>
+              <br />
+
+              <div className="absolute bottom-0 left-0 right-0 m-1 text-center">
+                <a
+                  className="inline-flex items-center justify-center px-3 py-2 text-sm font-medium text-center capitalize rounded-lg text-neutral-50 bg-neutral-700 neutral:bg-neutral-900 focus:ring-4 neutral:ring-neutral-700"
+                  href="https://gatsby-recipe-page.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Visit my site
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="m-4 sm:flex">
+          <div className="relative border rounded-lg shadow-md max-w-fit bg-neutral-100 border-neutral-500">
+            <div className="p-5">
+              <h5 className="mb-2 text-lg font-bold text-center text-gray-900 capitalize">
+                Everyday Iron Workout Warehouse
+              </h5>
+              <p className="mb-3 text-sm text-center text-neutral-900">
+                Open source code for Everyday Iron Workout Warehouse Gym in
+                Fresno, California. I develop there website, assist with other
+                tech and internet duties and consult them on there social media
+                sites. I also workout here and help assist coaches.
+              </p>
+              <br />
+
+              <div className="absolute bottom-0 left-0 right-0 m-1 text-center">
+                <a
+                  className="inline-flex items-center justify-center px-3 py-2 text-sm font-medium text-center capitalize rounded-lg text-neutral-50 bg-neutral-700 neutral:bg-neutral-900 focus:ring-4 neutral:ring-neutral-700"
+                  href="https://milliorn.github.io/Everyday-Iron/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  See my work
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="p-1 sm:flex">
+        <div className="flex-1 leading-loose bg-neutral-900 sm:order-2">
+          <div className="p-4 mx-auto my-0">
+            <h2 className="text-3xl leading-loose text-center capitalize">
+              what i enjoy doing
+            </h2>
+            <h3 className="text-lg font-semibold leading-loose text-center">
+              Click an icon to see my work.
+            </h3>
+          </div>
+          <p className="hidden m-4 leading-loose sm:flex">
+            My experience with Javascript is mostly with ReactJS. My CSS
+            experience beyond vanilla is with Bootstrap and Tailwind. These days
+            I prefer to use Tailwind, even with vanilla HTML sites. GatsbyJS
+            sites I created are hosted on Netlify and Gatsby Cloud. I have video
+            game experience with C#/.NET using Docker for continuous deployment
+            along with NWScript. Open source contributor for a video game .NET
+            API. Recently I have been learning Rust.
           </p>
         </div>
-        <div className="flex items-center justify-center w-full">
-          <div class="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-8 gap-4 p-4">
-            <div class=" text-cyan-300 text-7xl font-bold text-center p-4 border">
-              <DiReact />
-            </div>
-            <div class=" text-black bg-amber-300 text-7xl font-bold text-center p-4">
-              <DiJavascript1 />
-            </div>
-            <div class=" text-white bg-orange-600 text-7xl font-bold text-center p-4">
-              <DiHtml5 />
-            </div>
-            <div class=" text-white bg-lightBlue-700 text-7xl font-bold text-center p-4">
-              <DiCss3 />
-            </div>
-            <div class=" text-white bg-green-600 text-7xl font-bold text-center p-4">
-              <SiCsharp />
-            </div>
-            <div class=" text-white bg-lightBlue-400 text-7xl font-bold text-center p-4">
-              <SiTailwindcss />
-            </div>
-            <div class=" text-white bg-purple-800 text-7xl font-bold text-center p-4">
-              <SiGatsby />
-            </div>
-            <div class=" text-white bg-purple-700 text-7xl font-bold text-center p-4">
-              <SiBootstrap />
+        <div className="flex-1 p-4 bg-neutral-900">
+          <div className="flex items-center justify-center w-full">
+            <div className="grid grid-cols-2 gap-4 pb-4 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6">
+              <div className="p-4 text-black bg-amber-300 text-7xl">
+                <a
+                  href="https://github.com/milliorn?tab=repositories&q=&type=public&language=javascript&sort="
+                  target="_blank"
+                  rel="noreferrer"
+                  className="cursor-pointer"
+                >
+                  <DiJavascript1 />
+                </a>
+              </div>
+              <div className="p-4 text-center text-white bg-orange-600 text-7xl">
+                <a
+                  href="https://github.com/milliorn?tab=repositories&q=&type=public&language=html&sort="
+                  target="_blank"
+                  rel="noreferrer"
+                  className="cursor-pointer"
+                >
+                  <DiHtml5 />
+                </a>
+              </div>
+              <div className="p-4 text-center text-white bg-lightBlue-700 text-7xl">
+                <a
+                  href="https://github.com/milliorn?tab=repositories&q=css&type=public&language=&sort="
+                  target="_blank"
+                  rel="noreferrer"
+                  className="cursor-pointer"
+                >
+                  <DiCss3 />
+                </a>
+              </div>
+              <div className="p-4 text-center bg-white border-2 text-lightBlue-500 text-7xl">
+                <a
+                  href="https://github.com/milliorn?tab=repositories&q=react&type=public&language=&sort="
+                  target="_blank"
+                  rel="noreferrer"
+                  className="cursor-pointer"
+                >
+                  <DiReact />
+                </a>
+              </div>
+              <div className="p-4 text-center text-white bg-green-600 text-7xl">
+                <a
+                  href="https://github.com/milliorn?tab=repositories&q=&type=public&language=c%23&sort="
+                  target="_blank"
+                  rel="noreferrer"
+                  className="cursor-pointer"
+                >
+                  <SiCsharp />
+                </a>
+              </div>
+              <div className="p-4 text-center bg-white border-2 text-lightBlue-500 text-7xl">
+                <a
+                  href="https://github.com/milliorn?tab=repositories&q=tailwind&type=public&language=&sort="
+                  target="_blank"
+                  rel="noreferrer"
+                  className="cursor-pointer"
+                >
+                  <SiTailwindcss />
+                </a>
+              </div>
+              <div className="p-4 text-center text-white bg-purple-700 text-7xl">
+                <a
+                  href="https://github.com/milliorn?tab=repositories&q=gatsby&type=public&language=&sort="
+                  target="_blank"
+                  rel="noreferrer"
+                  className="cursor-pointer"
+                >
+                  <SiGatsby />
+                </a>
+              </div>
+              <div className="p-4 text-center text-white bg-purple-800 text-7xl">
+                <a
+                  href="https://github.com/milliorn?tab=repositories&q=bootstrap&type=public&language=&sort="
+                  target="_blank"
+                  rel="noreferrer"
+                  className="cursor-pointer"
+                >
+                  <SiBootstrap />
+                </a>
+              </div>
+              <div className="p-4 text-center text-white bg-orange-700 text-7xl">
+                <a
+                  href="https://github.com/milliorn?tab=repositories&q=postcss&type=public&language=&sort="
+                  target="_blank"
+                  rel="noreferrer"
+                  className="cursor-pointer"
+                >
+                  <SiPostcss />
+                </a>
+              </div>
+              <div className="p-4 text-center text-black bg-white border-2 text-7xl">
+                <a
+                  href="https://github.com/milliorn?tab=repositories&q=&type=public&language=rust&sort="
+                  target="_blank"
+                  rel="noreferrer"
+                  className="cursor-pointer"
+                >
+                  <SiRust />
+                </a>
+              </div>
+              <div className="p-4 text-center bg-white border-2 text-lightBlue-500 text-7xl">
+                <a
+                  href="https://github.com/milliorn?tab=repositories&q=docker&type=public&language=&sort="
+                  target="_blank"
+                  rel="noreferrer"
+                  className="cursor-pointer"
+                >
+                  <SiDocker />
+                </a>
+              </div>
+              <div className="p-4 text-center text-white bg-orange-600 text-7xl">
+                <a
+                  href="https://github.com/milliorn"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="cursor-pointer"
+                >
+                  <SiGit />
+                </a>
+              </div>
             </div>
           </div>
         </div>
