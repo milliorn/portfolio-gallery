7defc894210b32d55e63c158566a320657db4093
Scott Milliorn <scottmilliorn@gmail.com>
2022-04-11 21:34:21 -0700

    Skills

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,21 +1,9 @@
 import { useRef } from "react";
 import {
-  SiBootstrap,
-  SiCsharp,
-  SiCss3,
-  SiDocker,
-  SiGatsby,
-  SiGit,
   SiGithub,
   SiGmail,
-  SiHtml5,
-  SiJavascript,
   SiLeetcode,
   SiLinkedin,
-  SiPostcss,
-  SiReact,
-  SiRust,
-  SiTailwindcss,
   SiTwitter,
 } from "react-icons/si";
 import "./App.css";
@@ -25,6 +13,7 @@ import Hero from "./components/Hero";
 import NavBar from "./components/Navbar";
 import aboutImage from "./images/about.jpg";
 import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 export default function App() {
   const homeRef = useRef();
@@ -60,183 +49,40 @@ export default function App() {
         onClick={onClick}
         spanClassName="text-sm"
       />
+
       <Hero id="hero" spanText="React Developer" />
 
-      <br />
+      <br ref={aboutRef} />
 
-      <div ref={aboutRef}>
-        <About
-          span="Scott Milliorn"
-          paragraph="Passionate developer. Experienced game developer. Focused on mobile
+      <About
+        span="Scott Milliorn"
+        paragraph="Passionate developer. Experienced game developer. Focused on mobile
             first web development using React and Tailwind. Want to know more?
             Download my resume by clicking on the button."
-          download="PSM-Resume"
-          href={resumePDF}
-          target="_blank"
-          rel="noreferrer"
-          text="resume"
-          img={aboutImage}
-          alt="about"
-        />
-      </div>
+        download="PSM-Resume"
+        href={resumePDF}
+        target="_blank"
+        rel="noreferrer"
+        text="resume"
+        img={aboutImage}
+        alt="about"
+      />
 
-      <div ref={projectRef}>
-        <br />
-      </div>
+      <br ref={projectRef} />
 
       <Projects h2="open source projects" />
 
-      <div ref={skillsRef}>
-        <br />
-      </div>
+      <br ref={skillsRef} />
 
-      <section className="p-1 sm:flex" id="skills">
-        <div className="flex-1 leading-loose bg-neutral-900 sm:order-2">
-          <div className="p-4 mx-auto my-0">
-            <h2 className="text-3xl leading-loose text-center capitalize">
-              what i enjoy doing
-            </h2>
-            <h3 className="text-lg font-semibold leading-loose text-center">
-              Click an icon to see my work.
-            </h3>
-          </div>
-          <p className="hidden m-4 leading-loose sm:flex">
-            My experience with Javascript is mostly with ReactJS. My CSS
-            experience beyond vanilla is with Bootstrap and Tailwind. These days
-            I prefer to use Tailwind, even with vanilla HTML sites. GatsbyJS
-            sites I created are hosted on Netlify and Gatsby Cloud. I have video
-            game experience with C#/.NET using Docker for continuous deployment
-            along with NWScript. Open source contributor for a video game .NET
-            API. Recently I have been learning Rust.
-          </p>
-        </div>
-        <div className="flex-1 p-4 bg-neutral-900">
-          <div className="flex items-center justify-center w-full">
-            <div className="grid grid-cols-2 gap-4 pb-4 md:grid-cols-3 lg:grid-cols-4">
-              <div className="p-4 text-black bg-amber-300 text-7xl">
-                <a
-                  href="https://github.com/milliorn?tab=repositories&q=&type=public&language=javascript&sort="
-                  target="_blank"
-                  rel="noreferrer"
-                  className="cursor-pointer"
-                >
-                  <SiJavascript />
-                </a>
-              </div>
-              <div className="p-4 text-center text-white bg-orange-600 text-7xl">
-                <a
-                  href="https://github.com/milliorn?tab=repositories&q=&type=public&language=html&sort="
-                  target="_blank"
-                  rel="noreferrer"
-                  className="cursor-pointer"
-                >
-                  <SiHtml5 />
-                </a>
-              </div>
-              <div className="p-4 text-center text-white bg-lightBlue-700 text-7xl">
-                <a
-                  href="https://github.com/milliorn?tab=repositories&q=css&type=public&language=&sort="
-                  target="_blank"
-                  rel="noreferrer"
-                  className="cursor-pointer"
-                >
-                  <SiCss3 />
-                </a>
-              </div>
-              <div className="p-4 text-center bg-white border-2 text-lightBlue-500 text-7xl">
-                <a
-                  href="https://github.com/milliorn?tab=repositories&q=react&type=public&language=&sort="
-                  target="_blank"
-                  rel="noreferrer"
-                  className="cursor-pointer"
-                >
-                  <SiReact />
-                </a>
-              </div>
-              <div className="p-4 text-center text-white bg-green-600 text-7xl">
-                <a
-                  href="https://github.com/milliorn?tab=repositories&q=&type=public&language=c%23&sort="
-                  target="_blank"
-                  rel="noreferrer"
-                  className="cursor-pointer"
-                >
-                  <SiCsharp />
-                </a>
-              </div>
-              <div className="p-4 text-center bg-white border-2 text-lightBlue-500 text-7xl">
-                <a
-                  href="https://github.com/milliorn?tab=repositories&q=tailwind&type=public&language=&sort="
-                  target="_blank"
-                  rel="noreferrer"
-                  className="cursor-pointer"
-                >
-                  <SiTailwindcss />
-                </a>
-              </div>
-              <div className="p-4 text-center text-white bg-purple-700 text-7xl">
-                <a
-                  href="https://github.com/milliorn?tab=repositories&q=gatsby&type=public&language=&sort="
-                  target="_blank"
-                  rel="noreferrer"
-                  className="cursor-pointer"
-                >
-                  <SiGatsby />
-                </a>
-              </div>
-              <div className="p-4 text-center text-white bg-purple-800 text-7xl">
-                <a
-                  href="https://github.com/milliorn?tab=repositories&q=bootstrap&type=public&language=&sort="
-                  target="_blank"
-                  rel="noreferrer"
-                  className="cursor-pointer"
-                >
-                  <SiBootstrap />
-                </a>
-              </div>
-              <div className="p-4 text-center text-white bg-orange-700 text-7xl">
-                <a
-                  href="https://github.com/milliorn?tab=repositories&q=postcss&type=public&language=&sort="
-                  target="_blank"
-                  rel="noreferrer"
-                  className="cursor-pointer"
-                >
-                  <SiPostcss />
-                </a>
-              </div>
-              <div className="p-4 text-center text-black bg-white border-2 text-7xl">
-                <a
-                  href="https://github.com/milliorn?tab=repositories&q=&type=public&language=rust&sort="
-                  target="_blank"
-                  rel="noreferrer"
-                  className="cursor-pointer"
-                >
-                  <SiRust />
-                </a>
-              </div>
-              <div className="p-4 text-center bg-white border-2 text-lightBlue-500 text-7xl">
-                <a
-                  href="https://github.com/milliorn?tab=repositories&q=docker&type=public&language=&sort="
-                  target="_blank"
-                  rel="noreferrer"
-                  className="cursor-pointer"
-                >
-                  <SiDocker />
-                </a>
-              </div>
-              <div className="p-4 text-center text-white bg-orange-600 text-7xl">
-                <a
-                  href="https://github.com/milliorn"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="cursor-pointer"
-                >
-                  <SiGit />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Skills
+        h2="what i enjoy doing"
+        h3="Click an icon to see my work."
+        p="My experience with Javascript is mostly with ReactJS. My CSS experience beyond vanilla is with Bootstrap and Tailwind. These days I prefer to use Tailwind, even with vanilla HTML sites. GatsbyJS sites I
+          created are hosted on Netlify and Gatsby Cloud. I have video game
+          experience with C#/.NET using Docker for continuous deployment along
+          with NWScript. Open source contributor for a video game .NET API.
+          Recently I have been learning Rust."
+      />
 
       <footer className="m-1 bg-neutral-900" id="contact" ref={contactRef}>
         <h2 className="m-4 text-2xl text-center capitalize">
--- portfolio/src/assets/data.js
+++ portfolio/src/assets/data.js
@@ -1,3 +1,18 @@
+import {
+  SiBootstrap,
+  SiCsharp,
+  SiCss3,
+  SiDocker,
+  SiGatsby,
+  SiGit,
+  SiHtml5,
+  SiJavascript,
+  SiPostcss,
+  SiReact,
+  SiRust,
+  SiTailwindcss,
+} from "react-icons/si";
+
 export const navData = [
   {
     id: "homeNav",
@@ -44,5 +59,68 @@ export const projectsData = [
     p: "Open source code for Everyday Iron Workout Warehouse Gym in Fresno, California. I develop there website, assist with other tech and internet duties and consult them on there social media sites. I also workout here and help assist coaches.",
     href: "https://milliorn.github.io/Everyday-Iron/",
     message: "see my work"
+  }
+]
+
+export const skillsData = [
+  {
+    href: "https://github.com/milliorn?tab=repositories&q=&type=public&language=javascript&sort=",
+    icon: <SiJavascript />,
+    className: "p-4 text-black bg-amber-300 text-7xl"
+  },
+  {
+    href: "https://github.com/milliorn?tab=repositories&q=&type=public&language=html&sort=",
+    icon: <SiHtml5 />,
+    className: "p-4 text-center text-white bg-orange-600 text-7xl"
+  },
+  {
+    href: "https://github.com/milliorn?tab=repositories&q=css&type=public&language=&sort=",
+    icon: <SiCss3 />,
+    className: "p-4 text-center text-white bg-lightBlue-700 text-7xl"
+  },
+  {
+    href: "https://github.com/milliorn?tab=repositories&q=react&type=public&language=&sort=",
+    icon: <SiReact />,
+    className: "p-4 text-center bg-white border-2 text-lightBlue-500 text-7xl"
+  },
+  {
+    href: "https://github.com/milliorn?tab=repositories&q=&type=public&language=c%23&sort=",
+    icon: <SiCsharp />,
+    className: "p-4 text-center text-white bg-green-600 text-7xl"
+  },
+  {
+    href: "https://github.com/milliorn?tab=repositories&q=tailwind&type=public&language=&sort=",
+    icon: <SiTailwindcss />,
+    className: "p-4 text-center bg-white border-2 text-lightBlue-500 text-7xl"
+  },
+  {
+    href: "https://github.com/milliorn?tab=repositories&q=gatsby&type=public&language=&sort=",
+    icon: <SiGatsby />,
+    className: "p-4 text-center text-white bg-purple-700 text-7xl"
+  },
+  {
+    href: "https://github.com/milliorn?tab=repositories&q=bootstrap&type=public&language=&sort=",
+    icon: <SiBootstrap />,
+    className: "p-4 text-center text-white bg-purple-800 text-7xl"
+  },
+  {
+    href: "https://github.com/milliorn?tab=repositories&q=postcss&type=public&language=&sort=",
+    icon: <SiPostcss />,
+    className: "p-4 text-center text-white bg-orange-700 text-7xl"
+  },
+  {
+    href: "https://github.com/milliorn?tab=repositories&q=&type=public&language=rust&sort=",
+    icon: <SiRust />,
+    className: "p-4 text-center text-black bg-white border-2 text-7xl"
+  },
+  {
+    href: "https://github.com/milliorn?tab=repositories&q=docker&type=public&language=&sort=",
+    icon: <SiDocker />,
+    className: "p-4 text-center bg-white border-2 text-lightBlue-500 text-7xl"
+  },
+  {
+    href: "https://github.com/milliorn",
+    icon: <SiGit />,
+    className: "p-4 text-center text-white bg-orange-600 text-7xl"
   }
 ]
