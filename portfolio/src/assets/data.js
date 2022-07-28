7bc46c006e41e8a619b926475aac6a4d1aab4249
milliorn <scottmilliorn@gmail.com>
2022-07-27 17:15:04 -0700

    current weather

--- portfolio/src/assets/data.js
+++ portfolio/src/assets/data.js
@@ -14,142 +14,148 @@ import {
   SiReact,
   SiRust,
   SiTailwindcss,
-  SiTwitter
+  SiTwitter,
 } from "react-icons/si";
-
 
 export const navData = [
   {
     id: "homeNav",
     spanFirst: "h",
-    spanLast: "ome"
+    spanLast: "ome",
   },
   {
     id: "aboutNav",
     spanFirst: "a",
-    spanLast: "bout"
+    spanLast: "bout",
   },
   {
     id: "projectNav",
     spanFirst: "p",
-    spanLast: "rojects"
+    spanLast: "rojects",
   },
   {
     id: "skillsNav",
     spanFirst: "s",
-    spanLast: "kills"
+    spanLast: "kills",
   },
   {
     id: "contactNav",
     spanFirst: "c",
-    spanLast: "ontact"
-  }
-]
+    spanLast: "ontact",
+  },
+];
 
 export const projectsData = [
+  {
+    h3: "Current Weather",
+    p: "Simple app that does a search by city. Get full detailed information about the current weather and 7 day forecast. This project was bootstrapped with React and Tailwind CSS. It is powered by OpenWeather, GeoDB Cities and Unsplash",
+    href: "https://github.com/milliorn/Current-Weather",
+    message: "Get the Weather",
+  },
   {
     h3: "Neverwinter Nights .NET",
     p: "Open source organization for Neverwinter Nights: Enhanced Edition. We developed an API called Anvil for the Neverwinter Nights Server. NWN.Masterlist is used to consume Master Server API. NWN.Core is used to interlop functions for NWNX .Net and wrap core functions. NWN.Native is a Low-level NWNXLib bindings for managed to native interop.",
     href: "https://github.com/nwn-dotnet",
-    message: "learn more"
+    message: "learn more",
   },
   {
     h3: "Gatsby Recipes",
     p: "Recipe website done in GatsbyJS full stack frontend framework. It uses Contentful CMS, source controlled on Github, built and previewed in Gatsby Cloud, deployed onto Netlify Serverless Jamstack. It also acts as my personal site for cooking recipes.",
     href: "https://gatsbyrecipes.gatsbyjs.io/",
-    message: "visit my site"
+    message: "visit my site",
   },
   {
     h3: "Everyday Iron",
     p: "Open source code for Everyday Iron Workout Warehouse Gym in Fresno, California. I develop there website, assist with other tech and internet duties and consult them on there social media sites. I also workout here and help assist coaches.",
     href: "https://everyday-iron.netlify.app/",
-    message: "see my work"
-  }
-]
+    message: "see my work",
+  },
+];
 
 export const skillsData = [
   {
     href: "https://github.com/milliorn?tab=repositories&q=&type=public&language=javascript&sort=",
     icon: <SiJavascript />,
-    className: "p-4 text-black bg-amber-300 text-7xl"
+    className: "p-4 text-black bg-amber-300 text-7xl",
   },
   {
     href: "https://github.com/milliorn?tab=repositories&q=&type=public&language=html&sort=",
     icon: <SiHtml5 />,
-    className: "p-4 text-center text-white bg-orange-600 text-7xl"
+    className: "p-4 text-center text-white bg-orange-600 text-7xl",
   },
   {
     href: "https://github.com/milliorn?tab=repositories&q=css&type=public&language=&sort=",
     icon: <SiCss3 />,
-    className: "p-4 text-center text-white bg-lightBlue-700 text-7xl"
+    className: "p-4 text-center text-white bg-lightBlue-700 text-7xl",
   },
   {
     href: "https://github.com/milliorn?tab=repositories&q=react&type=public&language=&sort=",
     icon: <SiReact />,
-    className: "p-4 text-center bg-white border-2 text-lightBlue-500 text-7xl"
+    className: "p-4 text-center bg-white border-2 text-lightBlue-500 text-7xl",
   },
   {
     href: "https://github.com/milliorn?tab=repositories&q=&type=public&language=c%23&sort=",
     icon: <SiCsharp />,
-    className: "p-4 text-center text-white bg-green-600 text-7xl"
+    className: "p-4 text-center text-white bg-green-600 text-7xl",
   },
   {
     href: "https://github.com/milliorn?tab=repositories&q=tailwind&type=public&language=&sort=",
     icon: <SiTailwindcss />,
-    className: "p-4 text-center bg-white border-2 text-lightBlue-500 text-7xl"
+    className: "p-4 text-center bg-white border-2 text-lightBlue-500 text-7xl",
   },
   {
     href: "https://github.com/milliorn?tab=repositories&q=gatsby&type=public&language=&sort=",
     icon: <SiGatsby />,
-    className: "p-4 text-center text-white bg-purple-700 text-7xl"
+    className: "p-4 text-center text-white bg-purple-700 text-7xl",
   },
   {
     href: "https://github.com/milliorn?tab=repositories&q=bootstrap&type=public&language=&sort=",
     icon: <SiBootstrap />,
-    className: "p-4 text-center text-white bg-purple-800 text-7xl"
+    className: "p-4 text-center text-white bg-purple-800 text-7xl",
   },
   {
     href: "https://github.com/milliorn?tab=repositories&q=postcss&type=public&language=&sort=",
     icon: <SiPostcss />,
-    className: "p-4 text-center text-white bg-orange-700 text-7xl"
+    className: "p-4 text-center text-white bg-orange-700 text-7xl",
   },
   {
     href: "https://github.com/milliorn?tab=repositories&q=&type=public&language=rust&sort=",
     icon: <SiRust />,
-    className: "p-4 text-center text-black bg-white border-2 text-7xl"
+    className: "p-4 text-center text-black bg-white border-2 text-7xl",
   },
   {
     href: "https://github.com/milliorn?tab=repositories&q=docker&type=public&language=&sort=",
     icon: <SiDocker />,
-    className: "p-4 text-center bg-white border-2 text-lightBlue-500 text-7xl"
+    className: "p-4 text-center bg-white border-2 text-lightBlue-500 text-7xl",
   },
   {
     href: "https://github.com/milliorn",
     icon: <SiGit />,
-    className: "p-4 text-center text-white bg-orange-600 text-7xl"
-  }
-]
+    className: "p-4 text-center text-white bg-orange-600 text-7xl",
+  },
+];
 
 export const footerData = [
   {
     href: "https://www.linkedin.com/in/scott-milliorn",
-    className: "mx-2 my-4 text-3xl bg-white text-lightBlue-600 sm:text-4xl border rounded border-lightBlue-600",
-    icon: <SiLinkedin />
+    className:
+      "mx-2 my-4 text-3xl bg-white text-lightBlue-600 sm:text-4xl border rounded border-lightBlue-600",
+    icon: <SiLinkedin />,
   },
   {
     href: "https://twitter.com/scottmilliorn",
     className: "mx-2 my-4 text-3xl rounded-full text-lightBlue-500 sm:text-4xl",
-    icon: <SiTwitter />
+    icon: <SiTwitter />,
   },
   {
     href: "https://github.com/milliorn",
-    className: "mx-2 my-4 text-3xl text-black bg-white sm:text-4xl rounded-full border",
-    icon: <SiGithub />
+    className:
+      "mx-2 my-4 text-3xl text-black bg-white sm:text-4xl rounded-full border",
+    icon: <SiGithub />,
   },
   {
     href: "https://leetcode.com/milliorn/",
     className: "mx-2 my-4 text-3xl text-yellow-600 sm:text-4xl",
-    icon: <SiLeetcode />
-
-  }
-]
+    icon: <SiLeetcode />,
+  },
+];
