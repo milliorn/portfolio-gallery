f01dd68b4b22419319490cb617709651681f58af
milliorn <scottmilliorn@gmail.com>
2022-12-27 22:20:24 -0800

    navbar Image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,18 @@
+import Image from "next/image";
+import NavLogo from "../public/assets/navbarLogo.png";
 // Global navigation bar
 function Navbar(): JSX.Element {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="fixed w-full h-22 bg-white shadow-xl z-50">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image
+          src={NavLogo}
+          alt="/"
+          width="125"
+          height="50"
+          className="cursor-pointer"
+        />{" "}
+      </div>
     </div>
   );
 }
