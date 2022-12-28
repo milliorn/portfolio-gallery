e7bd3af3edcda3a536098d6120438816ea3295b1
milliorn <scottmilliorn@gmail.com>
2022-12-28 00:24:37 -0800

    Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
 import NavLogo from "../public/assets/navbarLogo.png";
+import React, { useState, useEffect } from "react";
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
-import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { BsFillPersonLinesFill } from "react-icons/bs";
-
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 // Global navigation bar
 function Navbar(): JSX.Element {
+  const [nav, setNav] = useState(false);
+
+  // onclick handler
+  function handleNav(): void {
+    setNav(!nav);
+  }
+
   return (
     <div className="fixed w-full h-22 bg-white shadow-xl z-50">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
@@ -17,6 +24,7 @@ function Navbar(): JSX.Element {
           height="50"
           className="cursor-pointer"
         />
+
         <div>
           <ul className="hidden md:flex">
             <Link href="/">
@@ -39,8 +47,79 @@ function Navbar(): JSX.Element {
               </li>
             </Link>
           </ul>
-          <div className="md:hidden">
+          <div className="md:hidden" onClick={handleNav}>
             <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      {/* Overlay */}
+      <div
+        className={
+          nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""
+        }
+      >
+        <div
+          className={
+            nav
+              ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-white p-10 ease-in duration-500"
+              : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+          }
+        >
+          <div>
+            <div className="flex w-full items-center justify-between">
+              <Image src={NavLogo} width="87" height="35" alt="/" />
+              <div
+                className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
+                onClick={handleNav}
+              >
+                <AiOutlineClose />
+              </div>
+            </div>
+            <div className="border-b border-gray-300 my-4">
+              <p className="w-[85%] md:w-[90%] py-4">
+                Let&apos;s collaborate on web applications!
+              </p>
+            </div>
+          </div>
+          <div className="py-4 flex flex-col">
+            <ul className="uppercase">
+              <Link href="/">
+                <li className="py-4 text-sm">Home</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">About</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Skills</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Project</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Contact</li>
+              </Link>
+            </ul>
+            <div className="pt-40">
+              <p className="uppercase tracking-widest text-[#5651e5]">
+                Let&#39;s Connect
+              </p>
+              <div className="flex items-center justify-evenly my-4 w-full sm:w-[80%]">
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <FaGithub />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <FaLinkedinIn />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <AiOutlineMail />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <BsFillPersonLinesFill />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
