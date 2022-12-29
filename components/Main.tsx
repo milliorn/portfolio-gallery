f6500194c0230a47c16f2ff9a12ebdafd9a39b10
milliorn <scottmilliorn@gmail.com>
2022-12-28 21:30:16 -0800

    main

---
+++ components/Main.tsx
@@ -0,0 +1,42 @@
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+
+// Main component on front page
+function Main(): JSX.Element {
+  return (
+    <div className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <p className="uppercase text-sm tracking-widest text-gray-600">
+            Let&apos;s collaborate on web development!
+          </p>
+          <h1 className="py-4 text-gray-700">
+            Hello, I&apos;m <span className="text-[#5651e5]">Scott</span>
+          </h1>
+          <h1 className="py-4 text-gray-700">Web Developer</h1>
+          <p className="py-4 text-gray-600 max-w-[70%] m-auto">
+            Focused on building responsive, mobile first web applications while
+            learning back-end technologies.
+          </p>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaGithub />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaLinkedinIn />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <AiOutlineMail />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <BsFillPersonLinesFill />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import Main from "../components/Main";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Wed Developer Portfolio</title>
-        <meta name="description" content="My Web Developer Portfolio site, Scott Milliorn" />
+        <meta
+          name="description"
+          content="My Web Developer Portfolio site, Scott Milliorn"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+      <Main />
     </>
-  )
+  );
 }
