3a31d2b9a317c4e0f2e49fe5ff20662a58337a4c
milliorn <scottmilliorn@gmail.com>
2022-12-31 16:06:30 -0800

    ProjectItem

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,8 +1,22 @@
+import ProjectItem from "./ProjectItem";
 
-const Projects = () => {
+// projects component
+function Projects(): JSX.Element {
   return (
-    <div>Projects</div>
-  )
+    <div className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Projects
+        </p>
+        <h2 className="py-4">Projects I Enjoyed Developing</h2>
+        <div className="grid md:gird-col-2 gap-8">
+          <ProjectItem
+          
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 
 import About from "../components/About";
 import Hero from "../components/Hero";
+import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,6 +23,7 @@ export default function Home() {
       <Hero />
       <About />
       <Skills />
+      <Projects />
     </>
   );
 }
