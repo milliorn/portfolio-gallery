5942012d850efacd2fac32446dbd31f6fa8156e9
itsjagdeep <96605901+itsjagdeep@users.noreply.github.com>
2023-07-24 20:02:03 -0400

    Update heroData.tsx

--- components/heros/heroData.tsx
+++ components/heros/heroData.tsx
@@ -4,21 +4,21 @@ import { FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 export const heroData = [
   {
     label: "github",
-    href: "https://github.com/milliorn",
+    href: "https://github.com/itsjagdeep",
     style:
       "rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300",
     icon: <FaGithub />,
   },
   {
     label: "linkedin",
-    href: "https://www.linkedin.com/in/scott-milliorn/",
+    href: "https://www.linkedin.com/in/thisisjagdeep/",
     style:
       "rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 text-sky-600",
     icon: <FaLinkedinIn />,
   },
   {
     label: "twitter",
-    href: "https://twitter.com/scottmilliorn",
+    href: "https://twitter.com/i/flow/login?redirect_after_login=%2F%3Flang%3Den",
     style:
       "rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 text-sky-500",
     icon: <FaTwitter />,
@@ -26,7 +26,7 @@ export const heroData = [
 
   {
     label: "resume",
-    href: "https://milliorn.github.io/digital-resume/",
+    href: "https://github.com/itsjagdeep/BeMusic",
     style:
       "rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 text-pink-500",
     icon: <BsFillPersonLinesFill />,
