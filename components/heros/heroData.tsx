34468b7dd9453788c81d147ccc9f38867ff57e03
itsjagdeep <96605901+itsjagdeep@users.noreply.github.com>
2023-07-28 14:52:29 -0400

    Update heroData.tsx

--- components/heros/heroData.tsx
+++ components/heros/heroData.tsx
@@ -13,7 +13,7 @@ export const heroData = [
     label: "linkedin",
     href: "https://www.linkedin.com/in/thisisjagdeep/",
     style:
-      "rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 text-sky-600",
+      "rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300 text-sky-600",
     icon: <FaLinkedinIn />,
   },
   
