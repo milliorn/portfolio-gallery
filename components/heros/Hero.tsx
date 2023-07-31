fda49004ed003fb33567d37c31e12f045b8ea039
itsjagdeep <96605901+itsjagdeep@users.noreply.github.com>
2023-07-31 19:46:20 -0400

    Update Hero.tsx

--- components/heros/Hero.tsx
+++ components/heros/Hero.tsx
@@ -4,7 +4,7 @@ import { HeroCoontainer } from "./HeroCoontainer";
 function Hero(): JSX.Element {
   return (
     <div className="w-full h-screen text-center" id="hero">
-      <div className="max-w-7xl w-full h-full mx-auto p-2 flex justify-around items-center">
+      <div className="max-w-5xl w-full h-full mx-auto p-1 flex justify-center items-center">
         <HeroCoontainer />
       </div>
     </div>
