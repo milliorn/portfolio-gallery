d0af4534cdb60e1aa1830d924969052d55bf341f
itsjagdeep <96605901+itsjagdeep@users.noreply.github.com>
2023-07-31 19:15:27 -0400

    new changes

--- components/contacts/ContactButtonsContainer.tsx
+++ components/contacts/ContactButtonsContainer.tsx
@@ -5,7 +5,7 @@ import { contactButtons } from "./contactButtons";
 // container to hold contact buttons
 export default function ContactButtonsContainer(): JSX.Element {
   return (
-    <div className="flex space-x-1 items-center justify-between max-w-xs m-auto py-0">
+    <div className="flex items-center justify-between max-w-xs m-auto py-0">
       {contactButtons.map((e) => (
         <Link aria-label={e.label} href={e.href} target="_blank" key={e.label}>
           <ContactButton className={e.className} button={e.button} />
--- components/contacts/contactButtons.tsx
+++ components/contacts/contactButtons.tsx
@@ -3,13 +3,13 @@ import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 export const contactButtons = [
   {
     button: <FaGithub />,
-    className: "rounded-full m-auto shadow-lg shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300",
+    className: "rounded-full m-auto p-6 shadow-lg shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300",
     href: "https://github.com/itsjagdeep",
     label: "Github",
   },
   {
     button: <FaLinkedinIn />,
-    className: "rounded-full m-auto shadow-lg shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300 text-sky-600",
+    className: "rounded-full m-auto p-6 shadow-lg shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300 text-sky-600",
     href: "https://www.linkedin.com/in/thisisjagdeep/",
     label: "LinkedinIn",
   },
