e833c2d946ed4fc508f030bc5234e1b980fe80b0
itsjagdeep <96605901+itsjagdeep@users.noreply.github.com>
2023-07-31 18:31:51 -0400

    Update contactButtons.tsx

--- components/contacts/contactButtons.tsx
+++ components/contacts/contactButtons.tsx
@@ -3,7 +3,7 @@ import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 export const contactButtons = [
   {
     button: <FaGithub />,
-    className: "rounded-full m-2 p-1 sm:text-6xl",
+    className: "rounded-full m-1 p-1 sm:text-6xl",
     href: "https://github.com/itsjagdeep",
     label: "Github",
   },
