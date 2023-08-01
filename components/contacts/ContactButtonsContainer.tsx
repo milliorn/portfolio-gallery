bf86259f264fb58c5b30d56f1b9a21f0313ade72
itsjagdeep <96605901+itsjagdeep@users.noreply.github.com>
2023-07-31 21:33:37 -0400

    Update ContactButtonsContainer.tsx

--- components/contacts/ContactButtonsContainer.tsx
+++ components/contacts/ContactButtonsContainer.tsx
@@ -5,7 +5,7 @@ import { contactButtons } from "./contactButtons";
 // container to hold contact buttons
 export default function ContactButtonsContainer(): JSX.Element {
   return (
-    <div className="flex items-center justify-around max-w-xs m-auto py-4">
+    <div className="text-5xl flex justify-center gap-16 py-3">
       {contactButtons.map((e) => (
         <Link aria-label={e.label} href={e.href} target="_blank" key={e.label}>
           <ContactButton className={e.className} button={e.button} />
