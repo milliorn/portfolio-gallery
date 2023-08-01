06789a44d1e9d065139f7f342652c6a31273f56f
itsjagdeep <96605901+itsjagdeep@users.noreply.github.com>
2023-07-31 21:44:55 -0400

    new changes

--- components/heros/HeroLinks.tsx
+++ components/heros/HeroLinks.tsx
@@ -4,7 +4,7 @@ import { heroData } from "./heroData";
 // hero links component that creates buttons in hero
 export default function HeroLinks(): JSX.Element {
   return (
-    <div className="flex items-center justify-between m-auto py-4">
+    <div className="text-5xl flex justify-center gap-16 py-3">
       {heroData.map((hero) => (
         <Link
           key={hero.label}
